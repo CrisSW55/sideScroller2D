931ea6fbb2ec162ec83846bfc5e7215f0491d2e9
Crystian Guzman <crystianguzman55@gmail.com>
2024-11-24 18:24:12 -0800

    added jump to player, noticed a bug when jumping the player jumps twice sometimes. Need to fix this bug next time!

--- src/Player.java
+++ src/Player.java
@@ -18,8 +18,12 @@ public class Player extends Entity {
     boolean sword_Equipped = false;
     boolean sword_Hit = false;
     boolean player_FeetTileCollision = false;
+    boolean player_LeftMovingUp = false;
+    boolean player_RightMovingUp = false;
     public final int screenX;
     public final int screenY;
+    final int init_pos_LevelY = pos_LevelY;
+    final int heightJump = init_pos_LevelY - (height);
     public Player(int x, int y, int w, int h,String init_Direction,GamePanel gp) {
         super(x, y, w, h,init_Direction);
         this.screenX = gp.screen_Width/2;
@@ -28,7 +32,7 @@ public class Player extends Entity {
         //this.setBounds(this.pos_LevelX,this.pos_LevelY,this.width,this.height);
         this.speed = 5;
         playerImages =  new ArrayList<BufferedImage>();
-        this.gravity = 2;
+        this.gravity = 3;
         kH = new KeyHandler();
         mH = new MouseHandler();
     }
@@ -69,19 +73,18 @@ public class Player extends Entity {
         //Tile and Player collision
         for(int row = 0; row<gp.totalLevelRow;row++){
             for(int col = 0; col<gp.totalLevelCol; col++){
-                int levelX = col * gp.tile_Width;
-                int levelY = row * gp.tile_Height;
-                int screenX = levelX - pos_LevelX + this.screenX;
-                int screenY = levelY - pos_LevelY + this.screenY;
+                int level_Width = col * gp.tile_Width;
+                int level_Height = row * gp.tile_Height;
+                int screenX = level_Width - pos_LevelX + this.screenX;
+                int screenY = level_Height - pos_LevelY + this.screenY;
                 if(gp.tileMgr.tiles.get(gp.tileMgr.tileIndex[row][col]) != null){
-                    if (screenY <= pos_LevelY + (height+height/2)) {
+                    if (screenY <= pos_LevelY + (height + height/2 + 10) && screenY + (height/2)>= pos_LevelY) {
                         player_FeetTileCollision = true;
                         gp.tileMgr.tiles.get(gp.tileMgr.tileIndex[row][col]).collision = true;
-//                        System.out.println("playerTileCollision inside first if statement is true of inner column loop: " + player_FeetTileCollision);
+
                         System.out.println("player position Y: " + pos_LevelY);
-                        System.out.println("current tile position Y " + screenY +  " " + row +  " "+ col);
-//                        System.out.println("player position Y: " + pos_LevelY);
-//                        System.out.println("current tile position Y " + screenY +  " " + row +  " "+ col);
+                        System.out.println("current tile position Y: " + screenY +  "col: " + row +  " row: "+ col);
+
                         break;
 
                     }
@@ -115,32 +118,78 @@ public class Player extends Entity {
         }
     }
     public void update(){
+
         //Gravity on player, player and tile collision
-        if (!player_FeetTileCollision) {
-            pos_LevelY += gravity;
+        // Need to jumping bug jumps again after jumping once
+        if (!player_FeetTileCollision) {pos_LevelY += speed;}
+        else if(player_LeftMovingUp){
+            pos_LevelY -= speed;
+            if( pos_LevelY <= heightJump){player_FeetTileCollision = false; player_LeftMovingUp = false;}
         }
+        else if(player_RightMovingUp){
+            pos_LevelY -= speed;
+            if( pos_LevelY <= heightJump){player_FeetTileCollision = false; player_RightMovingUp = false;}
+        }
+        //Player sprite horizontal movements
+        // (kH.is_UpPressed && kH.is_LeftPressed) || (kH.is_UpPressed && kH.is_RightPressed)
+        if ( kH.is_RightPressed || kH.is_LeftPressed || kH.is_DownPressed || kH.is_UpPressed)  {
+
+            //Player moving attacking
+            if (mH.is_AttackPressed && sword_Equipped && kH.is_RightPressed) {
+                if (direction.equals("right")) {
+                    spriteNum = 8;
+                    direction = "right";
+                    pos_LevelX += speed;
+                }
+            }
+            else if (mH.is_AttackPressed && sword_Equipped && kH.is_LeftPressed) {
+                if (direction.equals("left")) {
+                    spriteNum = 9;
+                    direction = "left";
+                    pos_LevelX -= speed;
+                }
+            }
 
 
-        //Player sprite movements
-        if (kH.is_RightPressed || kH.is_LeftPressed || kH.is_UpPressed || kH.is_DownPressed) {
-            if (kH.is_LeftPressed) {direction = "left";pos_LevelX -= speed;}
-            if (kH.is_RightPressed) {direction = "right";pos_LevelX += speed;}
-            if (kH.is_UpPressed) {direction = "up";pos_LevelY -= speed;}
-            if (kH.is_DownPressed) {direction = "down";pos_LevelY += speed;}
+                //Diagonal movements
+                else if (kH.is_UpPressed  && kH.is_LeftPressed && !kH.is_RightPressed &&  !mH.is_AttackPressed && direction.equals("left")) {
+                   pos_LevelX -= speed;
+                   player_LeftMovingUp = true;
+                }
+                else if (kH.is_UpPressed  && kH.is_RightPressed && !kH.is_LeftPressed && !mH.is_AttackPressed && direction.equals("right")) {
+                   pos_LevelX += speed;
+                   player_RightMovingUp = true;
+                }
+
+                //Jump only!
+                else if (kH.is_UpPressed && !kH.is_RightPressed && !kH.is_LeftPressed && !mH.is_AttackPressed && direction.equals("left")) {
+                    player_LeftMovingUp = true;
+                }
+                else if (kH.is_UpPressed && !kH.is_RightPressed && !kH.is_LeftPressed && !mH.is_AttackPressed && direction.equals("right")) {
+                    player_RightMovingUp = true;
+                }
+
+
+            //Vertical and horizontal movements
+            else if (!kH.is_UpPressed && kH.is_LeftPressed && !mH.is_AttackPressed) {direction = "left";pos_LevelX -= speed;}
+            else if (!kH.is_UpPressed && kH.is_RightPressed && !mH.is_AttackPressed) {direction = "right";pos_LevelX += speed;}
+
+
             //spriteIndex is the times update get called in this case total 60 times
 
             spriteIndex++;
             //So for every spriteIndex > 10, updates per 10 times, the spriteNum changes!
-            if (direction.equals("left") && spriteIndex > 10) {
+            if (direction.equals("left") && spriteIndex > 10 && !mH.is_AttackPressed) {
                 spriteNum = (spriteNum % 3) + 5; // Cycle through left sprites
                 spriteIndex = 0;
             }
-            else if (direction.equals("right") && spriteIndex > 10) {
+            else if (direction.equals("right") && spriteIndex > 10 && !mH.is_AttackPressed) {
                 spriteNum = (spriteNum % 3) + 1; // Cycle through right sprites
                 spriteIndex = 0;
             }
         }
-        //Player attacking
+
+        //Player standing attacking
         else if (mH.is_AttackPressed && sword_Equipped) {
             if (direction.equals("right")) {
                 spriteNum = 8;
@@ -150,16 +199,12 @@ public class Player extends Entity {
             }
         }
 
-
         //Player standing
         else if(direction.equals("right")){
             // Handle right idle state
             spriteNum = 0; // Assuming 0 is for standing
         }
         else if(direction.equals("left")){
-            if (mH.is_AttackPressed) {
-                spriteNum = 9;
-            }
             // Handle left idle state
             spriteNum = 4; //Assuming 4 is for leftstanding
         }
@@ -170,37 +215,37 @@ public class Player extends Entity {
         switch (direction){
             case "right":
                 if(spriteNum == 0){
-                    currentImage = stand;
+                    currentImage = playerImages.get(0);
                 }
                 else if(spriteNum == 1){
-                    currentImage = run1;
+                    currentImage = playerImages.get(1);
                 }
                 else if(spriteNum == 2){
-                    currentImage = run2;
+                    currentImage = playerImages.get(2);
                 }
                 else if(spriteNum == 3){
-                    currentImage = run3;
+                    currentImage = playerImages.get(3);
                 }
                 else if(spriteNum == 8){
-                    currentImage = swordattack;
+                    currentImage = playerImages.get(8);
                 }
                 break;
 
             case "left":
                 if(spriteNum == 4){
-                    currentImage = leftstand;
+                    currentImage = playerImages.get(4);
                 }
                 if(spriteNum == 5){
-                    currentImage = leftrun1;
+                    currentImage = playerImages.get(5);
                 }
                 else if(spriteNum == 6){
-                    currentImage = leftrun2;
+                    currentImage = playerImages.get(6);
                 }
                 else if(spriteNum == 7){
-                    currentImage = leftrun3;
+                    currentImage = playerImages.get(7);
                 }
                 else if(spriteNum == 9){
-                    currentImage = leftswordattack;
+                    currentImage = playerImages.get(9);
                 }
                 break;
 
